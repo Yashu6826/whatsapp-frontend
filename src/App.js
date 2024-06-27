8f3ddf9fc801b11902cdd1208537e077ea9cf686
Yashu6826 <yashrajgupt6826a@gmail.com>
2024-06-28 00:06:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { lazy, Suspense } from 'react';
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+//components
+import UserProvider from './context/UserProvider';
+import AccountProvider from './context/AccountProvider';
+
+import Loader from './components/loader/Loader';
+
+const Messenger = lazy(() => import('./components/Messenger'));
+
+function App() {
+
+  const clientId = '704343113871-vcn94fpvm3t05ru5ru1v34m0m3ut6ovb.apps.googleusercontent.com';
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <UserProvider>
+        <AccountProvider>
+          <Suspense fallback={<Loader />}>
+            <Messenger/>
+          </Suspense>
+        </AccountProvider>
+      </UserProvider>
+    </GoogleOAuthProvider>
+  );
+}
+
+export default App;
